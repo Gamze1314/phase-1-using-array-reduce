ad3ae4f34a3c1ee6fd54210c8a80780b62e49203
gamze <gamzekandara@gmail.com>
2023-11-26 13:42:42 -0500

    Adds index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+let batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+
+
+let totalBatteries = batteryBatches.reduce((acc, value) => acc + value, 0);
+console.log(totalBatteries);
